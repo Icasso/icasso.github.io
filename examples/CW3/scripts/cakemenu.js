78833ac65a4ad25b69e742797d2139055bfd73a7
IsaacTsui <43038654+GlisTIsaac@users.noreply.github.com>
2020-06-05 21:44:58 +0800

    Jun 6 update: add Crossing Game and CW3

---
+++ examples/CW3/scripts/cakemenu.js
@@ -0,0 +1,48 @@
+function initMenu() {
+  return [
+    {
+      description: "Buccellator",
+      nutrition: [
+        { type: "Carbohydrate", percentage: 20 },
+        { type: "Protein", percentage: 7 },
+        { type: "Fat", percentage: 25 },
+        { type: "Cholesterol", percentage: 0 }
+      ],
+      imagefile: "../images/Buccellator.jpg",
+      price: 128
+    },
+    {
+      description: "Strawberry",
+      nutrition: [
+        { type: "Carbohydrate", percentage: 15 },
+        { type: "Protein", percentage: 10 },
+        { type: "Fat", percentage: 30 },
+        { type: "Cholesterol", percentage: 20 }
+      ],
+      imagefile: "../images/Strawberry.jpg",
+      price: 233
+    },
+    {
+      description: "French Cruller ",
+      nutrition: [
+        { type: "Carbohydrate", percentage: 40 },
+        { type: "Protein", percentage: 5 },
+        { type: "Fat", percentage: 25 },
+        { type: "Cholesterol", percentage: 10 }
+      ],
+      imagefile: "../images/FrenchCruller.jpg",
+      price: 25
+    },
+    {
+      description: "Pound Layer",
+      nutrition: [
+        { type: "Carbohydrate", percentage: 25 },
+        { type: "Protein", percentage: 10 },
+        { type: "Fat", percentage: 30 },
+        { type: "Cholesterol", percentage: 5 }
+      ],
+      imagefile: "../images/PoundLayer.jpg",
+      price: 215
+    }
+  ];
+}
---
+++ examples/CW3/scripts/index.js
@@ -0,0 +1,54 @@
+window.onload = init;
+const mymenu = initMenu();
+let currentCake = 0;
+
+function init() {
+  rotate();
+  rotvid();
+}
+
+function rotate() {
+  document.getElementById("cakeInfo").src = mymenu[currentCake].imagefile;
+  document.getElementById("cakename").innerHTML =
+    mymenu[currentCake].description;
+  document.getElementById("carbo").innerHTML =
+    mymenu[currentCake].nutrition[0].percentage;
+  document.getElementById("cakeInfo").alt = mymenu[currentCake].description;
+  document.getElementById("pro").innerHTML =
+    mymenu[currentCake].nutrition[1].percentage;
+  document.getElementById("fat").innerHTML =
+    mymenu[currentCake].nutrition[2].percentage;
+  document.getElementById("cholest").innerHTML =
+    mymenu[currentCake].nutrition[3].percentage;
+  document.getElementById("total").innerHTML =
+    mymenu[currentCake].nutrition[0].percentage +
+    mymenu[currentCake].nutrition[1].percentage +
+    mymenu[currentCake].nutrition[2].percentage +
+    mymenu[currentCake].nutrition[3].percentage;
+  currentCake++;
+  if (currentCake == mymenu.length) {
+    currentCake = 0;
+  }
+  setTimeout("rotate()", 3 * 1000);
+}
+
+//end of rotating stuffs
+
+//loop vid TODO: add 5seconds delay  onended
+function rotvid() {
+var videos = document.getElementById("video");
+var videoss = [
+  "http://courses.cs.cityu.edu.hk/cs2204/chocolate.mp4",
+  "http://courses.cs.cityu.edu.hk/cs2204/cakemaking-s.mp4"
+];
+var currentVideo = 0;
+videos.addEventListener("ended", function(e) {
+  currentVideo = ++currentVideo % videoss.length;
+  videos.src = videoss[currentVideo];
+  videos.play();
+});
+setInterval(currentVideo, 5000)
+}
+//end of loop vid
+
+//test
