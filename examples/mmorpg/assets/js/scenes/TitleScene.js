b1f3c435522b538aca01773d786a9cf1697ba3e7
IsaacTsui <43038654+GlisTIsaac@users.noreply.github.com>
2020-06-06 22:16:49 +0800

    MMORPG

---
+++ examples/mmorpg/assets/js/scenes/TitleScene.js
@@ -0,0 +1,30 @@
+class TitleScene extends Phaser.Scene {
+  constructor() {
+    super("Title");
+  }
+  create() {
+    // create title text
+    this.titleText = this.add.text(
+      this.scale.width / 2,
+      this.scale.height / 2 - 50,
+      "Minimal MMORPG",
+      { fontSize: "64px", fill: "#fff" }
+    );
+    this.titleText.setOrigin(0.5);
+
+    // create play game button
+    this.startGameButton = new UiButton(
+      this,
+      this.scale.width / 2,
+      this.scale.height * 0.65,
+      "button1",
+      "button2",
+      "Start",
+      this.startScene.bind(this, "Game")
+    );
+  }
+
+  startScene(targetScene) {
+    this.scene.start(targetScene);
+  }
+}
